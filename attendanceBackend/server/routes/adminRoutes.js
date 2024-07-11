a23ed7cda7c52d270f056e001c55bee893264f33
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-11 16:11:48 +0530

    Update adminRoutes.js

--- attendanceBackend/server/routes/adminRoutes.js
+++ attendanceBackend/server/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const router=express.router()
+const router=express.Router()
 const addEmployeeController=require("../apis/employee/employeeController")
 
 router.post('/employee/add',addEmployeeController.addEmployee)
