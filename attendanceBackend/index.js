7af4837142c2a9d2cd5ebd65f02eab218d785586
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-11 16:08:19 +0530

    Update index.js

--- attendanceBackend/index.js
+++ attendanceBackend/index.js
@@ -1,6 +1,11 @@
 const express=require('express')
 const db=require('./server/config/db')
 const app=express()
+const adminRoute=require("./server/routes/adminRoutes")
+
+
+app.use("/admin",adminRoute)
+
 app.listen(3000,(err)=>{
     if(err){
         console.log("Error occured",err)
