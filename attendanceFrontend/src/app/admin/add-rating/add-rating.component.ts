a6c776ff485f768699df00862a10036d8fbbccfc
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-16 14:39:13 +0530

    jj

--- attendanceFrontend/src/app/admin/add-rating/add-rating.component.ts
+++ attendanceFrontend/src/app/admin/add-rating/add-rating.component.ts
@@ -3,46 +3,30 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { RatingService } from 'src/app/shared/rating/rating.service';
 
+interface Employee {
+  name: string;
+  rating: number;
+  remarks: string;
+}
 @Component({
   selector: 'app-add-rating',
   templateUrl: './add-rating.component.html',
   styleUrls: ['./add-rating.component.css']
 })
 export class AddRatingComponent implements OnInit {
-  employeeId!: string;  // definite assignment assertion
-  rating: number = 0;
-  ratingForm = new FormGroup({
-    remarks: new FormControl('')
-  });
+  employees: Employee[] = [];
 
-  constructor(private route: ActivatedRoute, private ratingService:RatingService) {}
+    ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
 
-  ngOnInit(): void {
-    this.employeeId = this.route.snapshot.paramMap.get('id')!;
-  }
+    
   
-
-  setRating(rating: number): void {
-    this.rating = rating;
-  }
-
-  addRating(): void {
-    const formValue = this.ratingForm.value;
-    const ratingData = {
-      employeeId: this.employeeId,
-      rating: this.rating,
-      remarks: formValue.remarks
-    };
-
-    this.ratingService.addRatingapi(ratingData).subscribe(
-      (res:any) => {
-        console.log('Rating added successfully', res);
-        // You can add more logic here to handle success response
-      },
-      (err: any)=> {
-        console.error('Error adding rating', err);
-        // Handle the error response here
-      }
-    );
-  }
-}
+    addEmployee() {
+      this.employees.push({ name: '', rating: 0, remarks: '' });
+    }
+  
+    removeEmployee(index: number) {
+      this.employees.splice(index, 1);
+    }}
+  
