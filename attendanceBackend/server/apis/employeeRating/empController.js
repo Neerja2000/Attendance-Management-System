8306f8d42b88fd5d9040ab1e81a9f75e0355cca2
Neerja2000 <neerjathakur7004@gmail.com>
2024-07-16 18:01:43 +0600

    hh

--- attendanceBackend/server/apis/employeeRating/empController.js
+++ attendanceBackend/server/apis/employeeRating/empController.js
@@ -1,6 +1,28 @@
 const Employee = require('./employeeModel');
 
 const addRating=async(req,res)=>{
-    
+    let total=await rating.countDocuments()
+    let newRating = new rating({
+        ratingId:total+1,
+        rating:req.body.rating,
+        remarks:req.body.remarks,
+        employeeId:req.body.employeeId
+        
+    })
+    newRating.save()
+    .then((result)=>{
+        res.json({
+            success:true,
+            status:200,
+            message:"Rating Added Successfully",
+            data:result
+        })
+    }).catch(err=>{
+        res.json({
+            success:false,
+            status:400,
+            message:err.message
+        })
+    })  
 }
 module.exports={addRating}
