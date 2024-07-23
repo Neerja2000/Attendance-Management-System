1d0a42f15965c2b59978492b2f4aa52f66a9d4a1
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-23 12:58:15 +0530

    Update auth.service.ts

--- attendanceFrontend/src/app/shared/auth/auth.service.ts
+++ attendanceFrontend/src/app/shared/auth/auth.service.ts
@@ -7,16 +7,19 @@ export class AuthService {
 
   constructor() { }
   storedata(res: any) {
-    // Check if res has the expected properties
-    if (res && res.token) {
-      // Assuming `_id` is available directly on `res` and not in `res.data`
-      sessionStorage.setItem('_id', res._id || ''); // Handle case where `_id` might be undefined
-      sessionStorage.setItem('token', res.token || ''); // Handle case where `token` might be undefined
+    // Check if response is for employee or admin login
+    if (res.data && res.data._id) {
+      // Handling employee login response
+      sessionStorage.setItem('_id', res.data._id || '');
+      sessionStorage.setItem('token', ''); // Employee login response doesn't include a token
+    } else if (res.token) {
+      // Handling admin login response
+      sessionStorage.setItem('_id', ''); // Admin login response doesn't include `_id`
+      sessionStorage.setItem('token', res.token || '');
     } else {
       console.error('Invalid response format:', res);
     }
   }
-  
 
   
 
