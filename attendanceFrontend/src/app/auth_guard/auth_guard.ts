ce2048560a829a6b461b32dfe5253b4348689ab2
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-24 11:04:04 +0530

    nhhk

---
+++ attendanceFrontend/src/app/auth_guard/auth_guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../shared/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class AuthGuard implements CanActivate{
+
+    constructor(private authservice:AuthService,private router:Router,private snackBar:MatSnackBar){}
+    canActivate():boolean{
+        if(this.authservice.getToken()==null)
+        {
+            this.snackBar.open('Login failed. Invalid Credientials.', 'Close', {
+                duration: 3000, // Duration in milliseconds
+                panelClass: ['error-snackbar'],
+                verticalPosition: 'top',
+                horizontalPosition: 'right'
+              });
+          this.router.navigateByUrl("/login")
+          return false
+        }
+        else
+        return true
+     }
+
+    
+  }
