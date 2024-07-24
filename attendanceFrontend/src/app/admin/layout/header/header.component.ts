1e4439fa498be1f6bfff03f42f342e2a0fcd6f69
sumalitakhi <sumalitakhi5260@gmail.com>
2024-07-24 11:25:25 +0530

    lkwhswqs

--- attendanceFrontend/src/app/admin/layout/header/header.component.ts
+++ attendanceFrontend/src/app/admin/layout/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 
+  constructor(private authservice:AuthService, private router:Router){
+
+  }
+  logout(){
+    this.authservice.removedata()
+   
+    this.router.navigateByUrl('/login')
+  }
 }
--- attendanceFrontend/src/app/employee/emp-layout/emp-header/emp-header.component.ts
+++ attendanceFrontend/src/app/employee/emp-layout/emp-header/emp-header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth/auth.service';
 
 @Component({
   selector: 'app-emp-header',
   templateUrl: './emp-header.component.html',
   styleUrls: ['./emp-header.component.css']
 })
-export class EmpHeaderComponent {
+export class EmpHeaderComponent implements OnInit {
 
+
+
+  
+  constructor(private authservice:AuthService, private router:Router){
+
+  }
+  ngOnInit(): void {
+    
+  }
+  logout(){
+    this.authservice.removedata()
+   
+    this.router.navigateByUrl('/login')
+  }
 }
