4d047a1016d3e35298bd43778683e8e35f8a2be5
Neerja2000 <neerjathakur7004@gmail.com>
2024-09-05 19:13:46 +0600

    fcgvbn

---
+++ attendanceBackend/server/apis/task/taskModel.js
@@ -0,0 +1,49 @@
+const mongoose=require('mongoose')
+
+const taskSchema=new mongoose.Schema({
+
+   taskId: {
+       type: Number,
+       required: true
+     },
+    
+      projectId:{
+        type:mongoose.Schema.Types.ObjectId,
+        default:null,
+        ref:'projects'
+      },
+    
+     taskName:{
+       type:String,
+       default:null,
+       required:true
+     },
+     taskDescription:{
+      type:String,
+      default:null
+      
+    },
+     expectedTime:{
+       type:String,
+       default:null
+       
+     },
+     file:{
+        type:String,
+        default:null
+        
+      },
+     createdAt: {
+        type: Date,
+        default: Date.now
+    
+      },
+      status: {
+        type: Boolean,
+        default: true
+      }
+
+})
+const task = mongoose.model('task', taskSchema);
+
+module.exports = task;
