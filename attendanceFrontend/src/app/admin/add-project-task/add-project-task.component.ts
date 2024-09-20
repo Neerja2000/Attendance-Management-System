9246e808d27202437e4cc0c11ed61c7c8766e94b
Neerja2000 <neerjathakur7004@gmail.com>
2024-09-20 18:45:23 +0530

    h

--- attendanceFrontend/src/app/admin/add-project-task/add-project-task.component.ts
+++ attendanceFrontend/src/app/admin/add-project-task/add-project-task.component.ts
@@ -78,7 +78,8 @@ export class AddProjectTaskComponent implements OnInit {
   getAllTasks() {
     this.taskService.getAllTaskProjectId(this._id).subscribe(
       (response: any) => {
-        this.tasks = response.data; // Assuming your API returns tasks in `data`
+        this.tasks = response.data;
+        this.getProjectBudget() // Assuming your API returns tasks in `data`
       },
       error => {
         console.error('Error fetching tasks', error);
